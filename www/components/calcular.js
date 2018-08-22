1d1c1d5e76a501d51f18c7eb3fbd8da9a5332254
Bruno111222333 <brunohenriquepbatista@gmail.com>
2018-08-23 03:42:47 +0900

    Project's Initial Commit

---
+++ www/components/calcular.js
@@ -0,0 +1,76 @@
+// This is a JavaScript file
+
+$(document).on("click", "#um", function(){
+    result+="1";
+    $("#result").val(1);
+    });
+    
+    $(document).on("click", "#dois", function(){
+    result+="2";
+    $("#result").val(2);
+    });
+    
+    $(document).on("click", "#tres", function(){
+    result+="3";
+    $("#result").val(3);
+    });
+
+    $(document).on("click", "#divisão", function(){
+    $("#result").val();
+    });
+    
+    $(document).on("click", "#quatro", function(){
+    result+="4";
+    $("#result").val(4);
+    });
+    
+    $(document).on("click", "#cinco", function(){
+    result+="5";
+    $("#result").val(5);
+    });
+    
+    $(document).on("click", "#seis", function(){
+    result+="6";
+    $("#result").val(6);
+    });
+
+    $(document).on("click", "#sub", function(){
+    $("#result").val();
+    });
+    
+    $(document).on("click", "#sete", function(){
+    result+="7";
+    $("#result").val(7);
+    });
+    
+    $(document).on("click", "#oito", function(){
+    result+="8";
+    $("#result").val(8);
+    });
+    
+    $(document).on("click", "#nove", function(){
+    result+="9";  
+    $("#result").val(9);
+    });
+
+    $(document).on("click", "#somar", function(){
+    $("#result").val();
+    });
+    
+    $(document).on("click", "#zero", function(){
+    result+="0";
+    $("#result").val(0);
+    });
+
+    $(document).on("click", "#ponto",  function(){
+    $("#result").val();
+    });
+
+    $(document).on("click", "#igual", function(){
+    $("#result").val();
+    });
+
+    $(document).on("click", "#mult", function(){
+    $("#result").val();
+    });
+
